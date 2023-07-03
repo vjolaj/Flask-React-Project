79c20a8782ba56a9df41dea0166b3d4ea51560c6
Genesis Mendes <genesismendes1@gmail.com>
2023-07-03 18:07:04 -0500

    add validators

--- react-app/src/store/restaurantsReducer.js
+++ react-app/src/store/restaurantsReducer.js
@@ -1,151 +1,167 @@
 //constants
 const GET_ALL_RESTAURANTS = "/restaurants/GET/all";
-const GET_SINGLE_RESTAURANT = '/restaurant/GET/single';
-const DELETE_SINGLE_RESTAURANT = '/restaurant/DELETE/single';
+const GET_SINGLE_RESTAURANT = "/restaurant/GET/single";
+const DELETE_SINGLE_RESTAURANT = "/restaurant/DELETE/single";
 
 //action creators
-const readAllRestaurantsAction = restaurants => {
-    return {
-        type: GET_ALL_RESTAURANTS,
-        restaurants
-    }
+const readAllRestaurantsAction = (restaurants) => {
+  return {
+    type: GET_ALL_RESTAURANTS,
+    restaurants,
+  };
 };
 
-const readSingleRestaurantAction = restaurant => {
-    return {
-        type: GET_SINGLE_RESTAURANT,
-        restaurant
-    }
+const readSingleRestaurantAction = (restaurant) => {
+  return {
+    type: GET_SINGLE_RESTAURANT,
+    restaurant,
+  };
 };
 
-const deleteRestaurantAction = restaurant => {
-    return {
-        type: DELETE_SINGLE_RESTAURANT,
-        restaurant
-    }
+const deleteRestaurantAction = (restaurant) => {
+  return {
+    type: DELETE_SINGLE_RESTAURANT,
+    restaurant,
+  };
 };
 
 //thunks
-export const getAllRestaurantsThunk = () => async dispatch => {
-    const res = await fetch('/api/restaurants')
+export const getAllRestaurantsThunk = () => async (dispatch) => {
+  const res = await fetch("/api/restaurants");
 
-    const data = await res.json();
+  const data = await res.json();
 
-    const normalizedData = {};
-    Object.values(data.all_restaurants).forEach(restaurant => {
-        normalizedData[restaurant.id] = restaurant
-    });
+  const normalizedData = {};
+  Object.values(data.all_restaurants).forEach((restaurant) => {
+    normalizedData[restaurant.id] = restaurant;
+  });
 
-    dispatch(readAllRestaurantsAction(normalizedData));
-    return data;
+  dispatch(readAllRestaurantsAction(normalizedData));
+  return data;
 };
 
-export const readSingleRestaurantThunk = (restaurant) => async dispatch => {
-    dispatch(readSingleRestaurantAction(restaurant));
+export const readSingleRestaurantThunk = (restaurant) => async (dispatch) => {
+  dispatch(readSingleRestaurantAction(restaurant));
 
-    return restaurant;
+  return restaurant;
 };
 
-export const createRestaurantThunk = (restaurant) => async dispatch => {
-    const { name, address, cuisine_type, price_range, imageUrl, rating, description } = restaurant;
+export const createRestaurantThunk = (restaurant) => async (dispatch) => {
+  const {
+    name,
+    address,
+    cuisine_type,
+    price_range,
+    imageUrl,
+    rating,
+    description,
+  } = restaurant;
 
-    const res = await fetch('/api/restaurants', {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-            name,
-            address,
-            cuisine_type,
-            price_range,
-            imageUrl,
-            rating,
-            description
-        })
-    });
+  const res = await fetch("/api/restaurants", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      name,
+      address,
+      cuisine_type,
+      price_range,
+      imageUrl,
+      rating,
+      description,
+    }),
+  });
 
-    const data = await res.json();
+  const data = await res.json();
 
-    //we can add menu items here or we can do this in another thunk if we end up using 2 pages for adding restaurant details and then menu
+  //we can add menu items here or we can do this in another thunk if we end up using 2 pages for adding restaurant details and then menu
 
-    return data;
+  return data;
 };
 
-export const editRestaurantThunk = (restaurant) => async dispatch => {
-    const { name, address, cuisine_type, price_range, imageUrl, rating, description } = restaurant;
+export const editRestaurantThunk = (restaurant) => async (dispatch) => {
+  const {
+    name,
+    address,
+    cuisine_type,
+    price_range,
+    imageUrl,
+    rating,
+    description,
+  } = restaurant;
 
-    const res = await fetch('/api/restaurants', {
-        method: "PUT",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-            name,
-            address,
-            cuisine_type,
-            price_range,
-            imageUrl,
-            rating,
-            description
-        })
-    });
+  const res = await fetch("/api/restaurants", {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      name,
+      address,
+      cuisine_type,
+      price_range,
+      imageUrl,
+      rating,
+      description,
+    }),
+  });
 
-    const data = await res.json();
-    return data;
+  const data = await res.json();
+  return data;
 };
 
-export const getUserRestaurantsThunk = () => async dispatch => {
-    const res = await fetch('/api/restaurants/current');
+export const getUserRestaurantsThunk = () => async (dispatch) => {
+  const res = await fetch("/api/restaurants/current");
 
-    const data = await res.json();
+  const data = await res.json();
 
-    const normalizedData = {};
-    Object.values(data.current_restaurants).forEach(restaurant => {
-        normalizedData[restaurant.id] = restaurant
-    });
+  const normalizedData = {};
+  Object.values(data.current_restaurants).forEach((restaurant) => {
+    normalizedData[restaurant.id] = restaurant;
+  });
 
-    dispatch(readAllRestaurantsAction(normalizedData));
-    return data;
+  dispatch(readAllRestaurantsAction(normalizedData));
+  return data;
 };
 
-export const deleteRestaurantThunk = (id) => async dispatch => {
-    const res = await fetch(`/api/restaurants/${id}/delete`, {
-        method: "DELETE"
-    });
+export const deleteRestaurantThunk = (id) => async (dispatch) => {
+  const res = await fetch(`/api/restaurants/${id}/delete`, {
+    method: "DELETE",
+  });
 
-    const data = await res.json()
+  const data = await res.json();
 
-    dispatch(deleteRestaurantAction(id));
+  dispatch(deleteRestaurantAction(id));
 
-    return data;
+  return data;
 };
 
 const initialState = {
-    singleRestaurant: {},
-    allRestaurants: {}
+  singleRestaurant: {},
+  allRestaurants: {},
 };
 
 const restaurantsReducer = (state = initialState, action) => {
-    switch (action.type) {
-        case GET_ALL_RESTAURANTS:
-            return {
-                ...state,
-                allRestaurants: action.restaurants
-            }
-        case GET_SINGLE_RESTAURANT:
-            return {
-                ...state,
-                singleRestaurant: action.restaurant
-        }
-        case DELETE_SINGLE_RESTAURANT:
-            const newState = {
-                ...state,
-                allRestaurants: {...state.allRestaurants}
-            }
-            delete newState.allRestaurants[action.id]
-            return newState
-    }
+  switch (action.type) {
+    case GET_ALL_RESTAURANTS:
+      return {
+        ...state,
+        allRestaurants: action.restaurants,
+      };
+    case GET_SINGLE_RESTAURANT:
+      return {
+        ...state,
+        singleRestaurant: action.restaurant,
+      };
+    case DELETE_SINGLE_RESTAURANT:
+      const newState = {
+        ...state,
+        allRestaurants: { ...state.allRestaurants },
+      };
+      delete newState.allRestaurants[action.id];
+      return newState;
+  }
 };
 
 export default restaurantsReducer;

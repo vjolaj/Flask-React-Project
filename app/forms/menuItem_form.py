004c83db12181a5daed5aa79b90705bc118543a3
vjolaj <vjolajorgji@gmail.com>
2023-07-04 12:30:59 -0400

    working through AWS, reviews routes

--- app/forms/menuItem_form.py
+++ app/forms/menuItem_form.py
@@ -1,6 +1,8 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, DecimalField, SelectField
 from wtforms.validators import DataRequired, Length, NumberRange, ValidationError
+from flask_wtf.file import FileField, FileAllowed, FileRequired
+from ..api.AWS_helpers import ALLOWED_EXTENSIONS
 
 def price_within_range(form, field):
     #Checking if price is within $0 to $100
@@ -13,5 +15,5 @@ class MenuItemForm(FlaskForm):
     price = DecimalField('Price', validators=[DataRequired(), price_within_range])
     itemType = SelectField('Item Type', choices=[('Entree', 'Entree'), ('Side', 'Side'), ('Dessert', 'Dessert'), ('Drink', 'Drink')])
     description = StringField('Item description', validators=[DataRequired(), Length(max=255)])
-    imageUrl = StringField('ImageUrl', validators=[DataRequired()])
+    imageUrl = FileField("Item Image", validators=[FileRequired(), FileAllowed(list(ALLOWED_EXTENSIONS))])
 

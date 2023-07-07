4f8d35449bac72967ed5c1dc6cbe993f8b89b78f
Genesis Mendes <genesismendes1@gmail.com>
2023-07-07 18:01:25 -0500

    css reviews validators, filters

--- react-app/src/components/AllMenuItems/index.js
+++ react-app/src/components/AllMenuItems/index.js
@@ -1,3 +1,113 @@
+// import React, { useState, useEffect, useRef } from "react";
+// import { useDispatch, useSelector } from "react-redux";
+// import "./AllMenuItems.css";
+// import { getAllMenuItemsThunk } from "../../store/menuItemsReducer";
+// import { useParams } from "react-router-dom/cjs/react-router-dom.min";
+// import ItemModal from "./itemModal";
+// import { receiveFilter, removeFilter } from "../../store/filterReducer";
+
+// export default function RestaurantMenuItems() {
+//   const { restaurantId } = useParams();
+//   const dispatch = useDispatch();
+//   const menuItems = useSelector((state) => state.menuItems.allMenuItems);
+//   const filter = useSelector((state) => state.filter.type);
+//   const modalRef = useRef();
+
+//   const [showMenu, setShowMenu] = useState(false);
+
+//   const openMenu = (menuItem) => {
+//     if (showMenu) return;
+//     setShowMenu(true);
+//   };
+
+//   useEffect(() => {
+//     if (!showMenu) return;
+
+//     const closeMenu = (e) => {
+//       if (!modalRef.current || !modalRef.current.contains(e.target)) {
+//         setShowMenu(false);
+//       }
+//     };
+
+//     document.addEventListener("click", closeMenu);
+
+//     return () => document.removeEventListener("click", closeMenu);
+//   }, [showMenu]);
+
+//   useEffect(() => {
+//     dispatch(getAllMenuItemsThunk(restaurantId));
+//   }, [dispatch, restaurantId]);
+
+//   const handleItemTypeChange = (itemType) => {
+//       dispatch(receiveFilter(itemType));
+//   };
+
+//   if (!menuItems) {
+//     return <div>Loading Menu Items...</div>;
+//   }
+
+//   const filteredMenuItems = filter ? Object.values(menuItems).filter((menuItem) => menuItem.itemType === filter): Object.values(menuItems);
+
+//   return (
+//     <div className="main-items">
+//       <div className="item-type-filter">
+//         <button
+//           onClick={() => handleItemTypeChange(null)}
+//           className={filter === null }
+//         >
+//           All
+//         </button>
+//         <button
+//           onClick={() => handleItemTypeChange("Entree")}
+//           className={filter === "Entree"}
+//         >
+//           Entrees
+//         </button>
+//         <button
+//           onClick={() => handleItemTypeChange("Side")}
+//           className={filter === "Side"}
+//         >
+//           Sides
+//         </button>
+//         <button
+//           onClick={() => handleItemTypeChange("Dessert")}
+//           className={filter === "Dessert"}
+//         >
+//           Desserts
+//         </button>
+//         <button
+//           onClick={() => handleItemTypeChange("Drink")}
+//           className={filter === "Drink"}
+//         >
+//           Beverages
+//         </button>
+//       </div>
+//        <div className="menuItemsContainer">
+//       {filteredMenuItems.length === 0 ? (
+//         <div className="no-items-message">
+//         </div>
+//       ) : (
+//         filteredMenuItems.map((menuItem) => (
+//           <div className="itemContainer" key={menuItem.id}>
+//             <div className="single-item-container">
+//               <div>
+//                 <img className="itemImage" src={menuItem.imageUrl} alt="image" />
+//                 <ItemModal menuItem={menuItem} />
+//                 <div className="itemImageContainer">
+//                   <h1 className="plus">+</h1>
+//                 </div>
+//               </div>
+//               <h3 className="itemName">{menuItem.itemName}</h3>
+//               <div className="itemName">${menuItem.price}</div>
+//             </div>
+//           </div>
+//         ))
+//       )}
+//     </div>
+//     </div>
+//   );
+// }
+
 import React, { useState, useEffect, useRef } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import "./AllMenuItems.css";
@@ -39,77 +149,74 @@ export default function RestaurantMenuItems() {
   }, [dispatch, restaurantId]);
 
   const handleItemTypeChange = (itemType) => {
-    if (itemType === filter) {
-      dispatch(removeFilter());
-    } else {
-      dispatch(receiveFilter(itemType));
-    }
+    dispatch(receiveFilter(itemType));
   };
 
   if (!menuItems) {
     return <div>Loading Menu Items...</div>;
   }
 
-  const filteredMenuItems = filter ? Object.values(menuItems).filter((menuItem) => menuItem.itemType === filter): Object.values(menuItems);
+  const filteredMenuItems = filter
+    ? Object.values(menuItems).filter((menuItem) => menuItem.itemType === filter)
+    : Object.values(menuItems);
 
   return (
     <div className="main-items">
       <div className="item-type-filter">
         <button
           onClick={() => handleItemTypeChange(null)}
-          className={filter === null ? "active" : ""}
+          className={filter === null}
         >
           All
         </button>
         <button
           onClick={() => handleItemTypeChange("Entree")}
-          className={filter === "Entree" ? "active" : ""}
+          className={filter === "Entree"}
         >
-          Entree
+          Entrees
         </button>
         <button
           onClick={() => handleItemTypeChange("Side")}
-          className={filter === "Side" ? "active" : ""}
+          className={filter === "Side"}
         >
-          Side
+          Sides
         </button>
         <button
           onClick={() => handleItemTypeChange("Dessert")}
-          className={filter === "Dessert" ? "active" : ""}
+          className={filter === "Dessert"}
         >
-          Dessert
+          Desserts
         </button>
         <button
           onClick={() => handleItemTypeChange("Drink")}
-          className={filter === "Drink" ? "active" : ""}
+          className={filter === "Drink"}
         >
-          Drink
+          Beverages
         </button>
       </div>
-       <div className="menuItemsContainer">
-      {filteredMenuItems.length === 0 ? (
-        <div className="no-items-message">
-          {filter ? `Sorry, there are no ${filter}s available at the moment.`: null}
-        </div>
-      ) : (
-        filteredMenuItems.map((menuItem) => (
-          <div className="itemContainer" key={menuItem.id}>
-            <div className="single-item-container">
-              <div>
-                <img className="itemImage" src={menuItem.imageUrl} alt="image" />
-                <ItemModal menuItem={menuItem} />
-                <div className="itemImageContainer">
-                  <h1 className="plus">+</h1>
-                </div>
-              </div>
-              <h3 className="itemName">{menuItem.itemName}</h3>
-              <div className="itemName">${menuItem.price}</div>
-            </div>
+      <div className="menuItemsContainer">
+        {filteredMenuItems.length === 0 ? (
+          <div className="no-items-message">
+            Sorry, there are no {filter}s available at the moment.
           </div>
-        ))
-      )}
-    </div>
+        ) : (
+          filteredMenuItems.map((menuItem) => (
+            <div className="itemContainer" key={menuItem.id}>
+              <div className="single-item-container">
+                <div>
+                  <img className="itemImage" src={menuItem.imageUrl} alt="image" />
+                  <ItemModal menuItem={menuItem} />
+                  <div className="itemImageContainer">
+                    <h1 className="plus">+</h1>
+                  </div>
+                </div>
+                <h3 className="itemName">{menuItem.itemName}</h3>
+                <div className="itemName">${menuItem.price}</div>
+              </div>
+            </div>
+          ))
+        )}
+      </div>
     </div>
   );
 }
-
--- react-app/src/components/PostReviewModal/index.js
+++ react-app/src/components/PostReviewModal/index.js
@@ -15,63 +15,70 @@ function ReviewModal({ restaurant }) {
   const [rating, setRating] = useState(0);
   const [errors, setErrors] = useState({});
   const [errorRes, setErrorRes] = useState({});
-//   const id = restaurant.restaurantId;
 
-    useEffect(() => {
-        const err = {};
-        if (description.length < 4) err.description = 'Review must be at least 10 character'
-        if (rating < 1) err.rating = 'Please select at least 1 star'
-        setErrors(err);
-    }, [description, rating])
+  useEffect(() => {
+    const err = {};
+    if (description.length < 10 && description.length > 1) err.description = 'Review must be between 10 and 250 characters';
+    if(description.length === 0) err.description ='';
+    if (description.length > 250) err.description = 'Sorry your review is too long';
+    if (rating < 1) err.rating = '';
+    setErrors(err);
+  }, [description, rating]);
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        setErrors({});
-        const newReview = {
-            rating,
-            description
-        }
-        const addReviewRes = await dispatch(createRestaurantReviewThunk(newReview, restaurant.id))
-        if (addReviewRes.message) {
-            await setErrorRes(addReviewRes)
-        }
-        await closeModal();
-        await dispatch(getRestaurantReviewsThunk(restaurant.id));
-        await dispatch(readSingleRestaurantThunk(restaurant.id))
-    }
-
-        const onChange = (rating) => {
-        setRating(parseInt(rating));
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setErrors({});
+    const newReview = {
+      rating,
+      description
     };
+    const addReviewRes = await dispatch(createRestaurantReviewThunk(newReview, restaurant.id));
+    if (addReviewRes.message) {
+      await setErrorRes(addReviewRes);
+    }
+    await closeModal();
+    await dispatch(getRestaurantReviewsThunk(restaurant.id));
+    await dispatch(readSingleRestaurantThunk(restaurant.id));
+  };
+
+  const onChange = (rating) => {
+    setRating(parseInt(rating));
+  };
 
   return (
-
-<>
-            <div className="reviewContainer">
-                <form className="reviewForm" onSubmit={handleSubmit}>
-                    <h1>Tell us how we did!</h1>
-                    {Boolean(Object.values(errorRes).length) ?
-                        <p className="error">{errorRes}</p> : null
-                    }
-                    <textarea
-                        cols='40'
-                        rows='7'
-                        placeholder="Leave your review here..."
-                        value={description}
-                        name="review"
-                        onChange={(e) => setDescription(e.target.value)}
-                    >
-                    </textarea>
-                    <div className="starsContainer">
-                        <StarsRatingInput disabled={false} onChange={onChange} rating={rating} />
-                        <p>Stars</p>
-                    </div>
-                    <button type="submit" className="submitReview" disabled={Boolean(Object.values(errors).length)}>Submit</button>
-                </form>
+    <>
+      <div className="reviewContainer">
+        <form className="reviewForm" onSubmit={handleSubmit}>
+          <h1>Tell us how we did!</h1>
+          {Boolean(Object.values(errors).length) && (
+            <div className="errorContainer">
+              {Object.values(errors).map((errorMsg, index) => (
+                <p key={index} className="error">{errorMsg}</p>
+              ))}
             </div>
-        </>
-
-  )
+          )}
+          <textarea
+            cols='40'
+            rows='7'
+            placeholder="Leave your review here..."
+            value={description}
+            name="review"
+            onChange={(e) => setDescription(e.target.value)}
+          ></textarea>
+          <div className="starsContainer">
+            <StarsRatingInput disabled={false} onChange={onChange} rating={rating} />
+            <p>Stars</p>
+          </div>
+          <button type="submit" className="submitReview" disabled={Boolean(Object.values(errors).length)}>Submit</button>
+        </form>
+      </div>
+    </>
+  );
 }
 
 export default ReviewModal;
+
+
+
+
+
